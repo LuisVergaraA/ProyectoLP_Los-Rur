0ee71da6f1b9987dd12ca2ef05bcf8b34bea3507
LuisVergaraA <150173288+LuisVergaraA@users.noreply.github.com>
2025-11-12 05:38:13 +0000

    Esqueleto del analizador léxico

--- analizador_lexico.py
+++ analizador_lexico.py
@@ -14,3 +14,34 @@ def find_column(lexpos: int) -> int:
         return 1
     last_nl = _code_text.rfind('\n', 0, lexpos)
     return (lexpos - last_nl) if last_nl >= 0 else (lexpos + 1)
+
+# SECCIÓN INTEGRANTE 1: Luis Vergara - LuisVergaraA
+
+
+# SECCIÓN INTEGRANTE 2
+
+
+# SECCIÓN INTEGRANTE 3
+
+# CONSOLIDACIÓN (temporal)
+reserved = {}
+tokens = []
+
+# Reglas comunes
+t_ignore = ' \t'
+
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += len(t.value)
+
+def t_error(t):
+    col = find_column(t.lexpos)
+    lexical_errors.append(
+        f"[Error Léxico] Línea {t.lineno}, col {col}: '{t.value[0]}'"
+    )
+    t.lexer.skip(1)
+
+# Construcción del lexer
+lexer = lex.lex()
+
+__all__ = ['tokens', 'lexer', 'lexical_errors', 'set_source']
