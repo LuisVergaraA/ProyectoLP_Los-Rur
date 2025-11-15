9ba6f14fca8f128f474da5ca2beda18021cc6be0
LuisVergaraA <150173288+LuisVergaraA@users.noreply.github.com>
2025-11-15 18:36:33 +0000

    Algoritmo de pruebas Integrante1 aumentado

--- algoritmos_kotlin/algoritmo_LuisVergaraA.kt
+++ algoritmos_kotlin/algoritmo_LuisVergaraA.kt
@@ -1,4 +1,6 @@
-// Prueba 1: Declaración de funciones básicas
+// AVANCE 1
+
+Prueba 1: Declaración de funciones básicas
 fun suma(a: Int, b: Int): Int {
     return a + b
 }
@@ -104,5 +106,140 @@ fun maximo(a: Int, b: Int): Int {
 
 
 // AVANCE 2: SINTAXIS COMPLEJA 
+
+// PRUEBA 1: DECLARACIÓN Y ASIGNACIÓN
+// Declaración con val (inmutable)
+val constante = 100;
+val pi = 3.14159;
+val nombre = "Juan";
+val activo = true;
+
+// Declaración con var (mutable)
+var contador = 0;
+var temperatura = 25.5;
+var mensaje = "Hola";
+var esValido = false;
+
+// Asignaciones múltiples
+var x = 10;
+x = 20;
+x = 30;
+
+var y = 5;
+y = y + 10;
+y = y * 2;
+
+
+// PRUEBA 2: EXPRESIONES ARITMÉTICAS
+// Operadores básicos
+val suma = 10 + 5;
+val resta = 20 - 8;
+val multiplicacion = 4 * 7;
+val division = 50 / 2;
+val modulo = 17 % 5;
+
+// Expresiones compuestas
+val resultado1 = 10 + 5 * 3;
+val resultado2 = (10 + 5) * 3;
+val resultado3 = 100 / (2 + 3);
+val resultado4 = 10 + 20 - 5 * 2;
+
+// Con paréntesis anidados
+val complejo1 = ((10 + 5) * (3 - 1)) / 2;
+val complejo2 = (50 - (10 + 5)) * 2;
+
+// Operadores unarios
+val negativo = -10;
+val dobleNegativo = -(-5);
+
+
+// PRUEBA 3: EXPRESIONES LÓGICAS
+// Operadores relacionales
+val esMayor = 10 > 5;
+val esMenor = 3 < 8;
+val esIgual = 5 == 5;
+val esDiferente = 7 != 3;
+val mayorIgual = 10 >= 10;
+val menorIgual = 5 <= 8;
+
+// Operadores lógicos AND
+val ambosVerdaderos = true && true;
+val unoFalso = true && false;
+val condicion1 = (10 > 5) && (20 < 30);
+val condicion2 = (x > 0) && (y > 0);
+
+// Operadores lógicos OR
+val alMenosUno = true || false;
+val ambos = false || false;
+val condicion3 = (x < 0) || (y < 0);
+val condicion4 = (temperatura > 30) || (temperatura < 0);
+
+// Operador de negación
+val noVerdadero = !true;
+val noFalso = !false;
+val negacion1 = !(10 > 5);
+val negacion2 = !(x == 0);
+
+// Combinaciones complejas
+val compleja1 = (10 > 5) && (20 < 30) || (x == 0);
+val compleja2 = !(x > 10) && (y < 20);
+val compleja3 = (a >= 0 && a <= 100) || (b >= 0 && b <= 100);
+
+
+// PRUEBA 4: IMPRESIÓN (println)
+println(42);
+println(3.14);
+println("Hola Mundo");
+println(true);
+println(x);
+println(x + y);
+println((10 + 5) * 2);
+println(x > 10);
+println("El resultado es:");
+
+
+// PRUEBA 5: ESTRUCTURAS DE CONTROL (if-else)
+// If simple
+if (x > 0) {
+    println("x es positivo");
+}
+
+// If-else
+if (temperatura > 30) {
+    println("Hace calor");
+} else {
+    println("Temperatura normal");
+}
+
+// If-else anidado
+if (x > 0) {
+    println("Positivo");
+} else {
+    if (x < 0) {
+        println("Negativo");
+    } else {
+        println("Cero");
+    }
+}
+
+// If-else como expresión
+val mensaje1 = if (x > 10) "Grande" else "Pequeño";
+val estado = if (activo) "Activo" else "Inactivo";
+
+// Condiciones complejas
+if ((x > 0) && (y > 0)) {
+    println("Ambos positivos");
+}
+
+if ((temperatura > 35) || (temperatura < 0)) {
+    println("Temperatura extrema");
+} else {
+    println("Temperatura normal");
+}
+
+if (!(x == 0) && (y != 0)) {
+    val division = x / y;
+    println(division);
+}
 
 
